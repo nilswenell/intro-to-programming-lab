bbe7bed57810cabbed7004b0a9e0e7830a234109
nilswenell <nils.wenell@gmail.com>
2025-10-21 15:36:30 -0500

    object oriented programming notes

---
+++ Week_9/OOP.py
@@ -0,0 +1,10 @@
+# OOP: Object Oriented Programming
+# every data object is an item in python.
+
+# classes describe what an object looks like.
+# example: int. the number 3 would be an instance of the 'int' class.
+# one of the things this class can do is undergo operations.
+
+x = 3 # x is an instance of an int
+x = x + 3 # this int did an operation by adding 3
+#     ^ this '+' is an alias for x.__add__(3) or something.
